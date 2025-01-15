6daf1a9cccc3022effcc0fe650724402b7e32836
Lijian <976683240@qq.com>
2025-01-15 14:51:24 +0800

    feat: 添加关键字并更新验证规则
    - 在 composer.json 中添加 webman、laravel、validator 关键字
    - 更新 test.php 中的验证规则，将 fff 字段的验证改为 isFloat(2)- 调整 aa、bb、cc、dd 字段的验证规则

--- test/test.php
+++ test/test.php
@@ -8,16 +8,16 @@ use Jeckleee\Tools\Validator as V;
 //die;
 
 $data = V::array([
-    'aa' => '10.1',
-    'bb' => '12',
-    'cc' => '622426199102230071',
-    'dd' => 'a',
-    'fff' => '1.011',
+	'aa' => '10.1',
+	'bb' => '12',
+	'cc' => '622426199102230071',
+	'dd' => 'a',
+	'fff' => '{"a":1}',
 ], [
-    V::field('fff')->ifExisted()->isFloat(2)->verify('请填写3位小数'),
-    //V::field('bb')->withRegex('/^[a-zA-Z]+$/')->verify('数据类验证失败'),
-    //V::field('cc')->isIdCard()->verify('请填写正确的身份证号'),
-    //V::field('ee')->required('eeeee')->verify()
+	V::field('cc')->ifExisted()->isIdCard()->verify('必须是身份证号'),
+	//V::field('bb')->withRegex('/^[a-zA-Z]+$/')->verify('数据类验证失败'),
+	//V::field('cc')->isIdCard()->verify('请填写正确的身份证号'),
+	//V::field('ee')->required('eeeee')->verify()
 ], Exception::class, 400);
 
 echo json_encode($data);
