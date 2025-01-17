2cbf0306044f5314989e11a023f025758c93c941
Lijian <976683240@qq.com>
2025-01-17 14:26:13 +0800

    feat(tools): 增加验证失败错误返回模式配置
    - 在 app.php 配置文件中添加 'error_return_mode' 选项
    - 可设置错误返回模式为 'collective'（集中返回）或 'immediate'（立即返回）

--- src/config/plugin/jeckleee/tools/app.php
+++ src/config/plugin/jeckleee/tools/app.php
@@ -1,10 +1,13 @@
 <?php
 return [
-    'enable' => true,
+	'enable' => true,
 
-    // 定义验证失败以后抛出的异常
-    'exception' => Exception::class,
+	// 定义验证失败以后抛出的异常
+	'exception' => Exception::class,
 
-    // 定义验证失败的错误码
-    'exception_code' => 500,
+	// 定义验证失败的错误码
+	'exception_code' => 500,
+
+	//验证失败错误如何返回(immediate:立即返回,集中返回:collective)
+	'error_return_mode' => 'collective',
 ];
