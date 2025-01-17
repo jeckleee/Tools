5c7556ca402804c7b0eb22c5c7e94e4e8c8519e3
Lijian <976683240@qq.com>
2025-01-17 14:27:22 +0800

    refactor(test): 重构测试代码并添加异常处理
    - 使用 try-catch 块包裹验证逻辑，以处理可能抛出的异常
    - 修改字段名 'fff' 为 'ff'，简化测试数据结构
    - 在验证规则中添加 JSON 格式验证
    - 调整验证失败时的错误信息和状态码

--- test/test.php
+++ test/test.php
@@ -6,18 +6,24 @@ use Jeckleee\Tools\Validator as V;
 
 //echo json_encode(Validator::fieldName('aa')->isNumber()->betweenNumber(1,10)->msg('数据类验证失败')->rules);
 //die;
+try {
+	$data = V::check([
+		'aa' => '10.1',
+		'bb' => '12',
+		'cc' => '622426199102230071',
+		'dd' => 'a',
+		'ff' => '{"a":1}',
+	], [
+		V::field('cc')->ifExisted()->isIdCard()->verify('请填写正确的身份证号', 300),
+		V::field('ff')->isJson()->verify('必须是json字符串', 301),
+		//V::field('cc')->isIdCard()->verify('请填写正确的身份证号'),
+		//V::field('ee')->required('eeeee')->verify()
+	]);
 
-$data = V::array([
-	'aa' => '10.1',
-	'bb' => '12',
-	'cc' => '622426199102230071',
-	'dd' => 'a',
-	'fff' => '{"a":1}',
-], [
-	V::field('cc')->ifExisted()->isIdCard()->verify('必须是身份证号'),
-	//V::field('bb')->withRegex('/^[a-zA-Z]+$/')->verify('数据类验证失败'),
-	//V::field('cc')->isIdCard()->verify('请填写正确的身份证号'),
-	//V::field('ee')->required('eeeee')->verify()
-], Exception::class, 400);
-
-echo json_encode($data);
+	echo json_encode($data);
+} catch (\Exception $e) {
+	echo json_encode([
+		'code' => $e->getCode(),
+		'msg' => $e->getMessage(),
+	]);
+}
